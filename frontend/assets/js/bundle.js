3a3b7d113d5ea2e9294b89243572c9fae45f397c
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2024-06-08 14:45:41 -0300

    first commit

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+
+console.log('main.ts is working');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.front.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+  experiments: {
+    topLevelAwait: true,
+  },
+};
