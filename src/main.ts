7df993c5f4925fa1116a6ef1b592fafd13cd90ef
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2024-06-11 14:08:13 -0300

    main.ts done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-console.log('main.ts is working')
+import { getGame } from './getGame';
+import { Game } from './interfaces/game';
+
+const $selector = document.querySelector('#platforms') as HTMLSelectElement;
+const $selectedGame = document.querySelector(
+  '#selected_game',
+) as HTMLDivElement;
+
+$selector.addEventListener(
+  'change',
+  async () => {
+    console.log('change!');
+    const path = `./assets/files/${$selector.value}.csv`;
+    const game = await getGame(path);
+
+    if (!game) return;
+
+    return showGame(game);
+  },
+  false,
+);
+
+function showGame(game: Game) {
+  const $gameName = $selectedGame.querySelector(
+    '#game_name',
+  ) as HTMLParagraphElement;
+  const $gameDeveloper = $selectedGame.querySelector(
+    '#game_developer',
+  ) as HTMLParagraphElement;
+
+  $gameName.textContent = `Title: ${game.Title}`;
+  $gameDeveloper.textContent = `Developer: ${game['Developer(s)']}`;
+}
